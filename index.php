b8746eeb11fdd73d14d173b550e072112d0421e0
williamenrique <william21enrique@gmail.com>
2020-09-13 16:49:17 -0400

    Inicializabdo

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include 'src/include/head.php'?>
+<!-- ======= Header ======= -->
+<!-- End Header -->
+
+<!-- ======= Hero Section ======= -->
+<!-- End Hero -->
+
+<main id="main">
+
+	<!-- ======= Cliens Section ======= -->
+	<!-- End Cliens Section -->
+
+	<!-- ======= About Us Section ======= -->
+	<!-- End About Us Section -->
+
+	<!-- ======= Skills Section ======= -->
+	<!-- End Skills Section -->
+
+	<!-- ======= Services Section ======= -->
+	<!-- End Services Section -->
+
+
+	<!-- ======= Pricing Section ======= -->
+	<!-- End Pricing Section -->
+
+	<!-- ======= Contact Section ======= -->
+	<!-- End Contact Section -->
+
+</main><!-- End #main -->
+
+<!-- ======= Footer ======= -->
+<!-- End Footer -->
+<?php include 'src/include/foot.php'?>
---
+++ src/include/foot.php
@@ -0,0 +1,16 @@
+<a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+<div id="preloader"></div>
+
+<!-- Vendor JS Files -->
+<script src="src/assets/jquery.min.js"></script>
+<script src="src/assets/bootstrap.bundle.min.js"></script>
+<!-- <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+<script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/venobox/venobox.min.js"></script>
+<script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+<script src="assets/vendor/aos/aos.js"></script> -->
+</body>
+
+</html>
